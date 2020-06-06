2ef8961a7c85796518cadded23bdfe199603a65f
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-06 22:01:34 +0530

    setting container into SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.red,
+        //Safe area takes up the space which is visible , ignoring the phone notch, etc
+        body: SafeArea(
+          child: Container(
+              //Container with no children try to be as big as possible
+              width: 100.0,
+              height: 100.0,
+              color: Colors.white,
+              //Containers with children, size themselves to their children
+              child: Text("Hello World!")),
+        ),
       ),
     );
   }
