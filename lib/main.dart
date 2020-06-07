ddb98b63c6c470ad77422c47731fa30b29c45235
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-07 17:59:38 +0530

    creating a column widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,37 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.red,
         //Safe area takes up the space which is visible , ignoring the phone notch, etc
         body: SafeArea(
-          child: Container(
-              //Container with no children try to be as big as possible
-              width: 100.0,
-              height: 100.0,
-              color: Colors.white,
-              margin: EdgeInsets.only(left: 30.0),
-              padding: EdgeInsets.all(20.0),
-              //Containers with children, size themselves to their children
-              child: Text("Hello World!")),
+          child: Column(
+            children: <Widget>[
+              Container(
+                  //Container with no children try to be as big as possible
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.white,
+                  margin: EdgeInsets.only(left: 30.0),
+                  padding: EdgeInsets.all(5.0),
+                  //Containers with children, size themselves to their children
+                  child: Text("Container 1")),
+              Container(
+                  //Container with no children try to be as big as possible
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.blue,
+                  margin: EdgeInsets.only(left: 30.0),
+                  padding: EdgeInsets.all(5.0),
+                  //Containers with children, size themselves to their children
+                  child: Text("Container 2")),
+              Container(
+                  //Container with no children try to be as big as possible
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                  margin: EdgeInsets.only(left: 30.0),
+                  padding: EdgeInsets.all(5.0),
+                  //Containers with children, size themselves to their children
+                  child: Text("Container 3"))
+            ],
+          ),
         ),
       ),
     );
