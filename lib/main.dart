4fad0b715cd46dcb5b1a9a9f4a5c99ca18a6b7e7
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-08 22:04:22 +0530

    adding a profile image

--- lib/main.dart
+++ lib/main.dart
@@ -12,51 +12,12 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         //Safe area takes up the space which is visible , ignoring the phone notch, etc
         body: SafeArea(
-          child: Row(
-            //mainAxisSize: MainAxisSize.min, //setting the size that the column should take up to minimum, by default it is maximum
-            //verticalDirection: VerticalDirection.up, // Direction in which the children would lay out on the vertical axis, up is towards upwards , by default it is downwards
-            mainAxisAlignment: MainAxisAlignment
-                .spaceBetween, //align the column axis from center, space children evenly,etc.
-            crossAxisAlignment: CrossAxisAlignment
-                .stretch, // for a column cross alignment is horizontal axis and vice versa for a row
+          child: Column(
             children: <Widget>[
-              Container(
-                //Container with no children try to be as big as possible
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/my-passport-photo.jpg'),
               ),
-              // specify width for spacing containers in row and height for column containers
-              Container(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green,
-                  ),
-                ],
-              )),
-
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.blue,
-                    )
-                  ],
-                ),
-              )
             ],
           ),
         ),
